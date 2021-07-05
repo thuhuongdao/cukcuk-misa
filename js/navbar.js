c9277e53276c56e5b0cb27a78f18bcaefe4a601b
HUONG DAO THU <dthuong@rd.local>
2021-07-05 14:23:55 +0700

    edit base

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,14 @@
-var navbar = document.getElementsByClassName("navbar");
-var navbarItems = navbar[0].getElementsByClassName("navbar-item");
-for (var i = 0; i < navbarItems.length; i++) {
-    navbarItems[i].addEventListener("click", function() {
-  var current = navbar.getElementsByClassName("active");
-  current[0].classList.remove("active");
-  this.classList.add("active");
-  });
-}
+document.addEventListener("DOMContentLoaded", function(){
+  var navbar = document.getElementsByClassName("navbar");
+  var navbarItems = navbar[0].getElementsByClassName("navbar-item");
+  for (var i = 0; i < navbarItems.length; i++) {
+      navbarItems[i].addEventListener("click", function() {
+          console.log("ccc");
+    var current = navbar[0].getElementsByClassName("active");
+    console.log(current);
+    current[0].classList.remove("active");
+    this.classList.add("active");
+    });
+  }
+})
+
